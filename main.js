b9ed5babf73bd2cc17a1a7b7482ef2f731ac90ad
DreamAndDead <favorofife@yeah.net>
2018-04-16 09:55:41 +0800

    migrate from test repo

---
+++ main.js
@@ -0,0 +1,442 @@
+/*
+ * external link: dreamanddead.github.io/playoff-guide
+ * or dreamanddead.github.io/playoff-guide?map=
+ */
+
+var map_init = {
+    "0000": {
+        "name": "HOU",
+        "score": "0"
+    },
+    "0001": {
+        "name": "MIN",
+        "score": "0"
+    },
+    "0010": {
+        "name": "OKC",
+        "score": "0"
+    },
+    "0011": {
+        "name": "UTA",
+        "score": "0"
+    },
+    "0100": {
+        "name": "POR",
+        "score": "0"
+    },
+    "0101": {
+        "name": "NOP",
+        "score": "0"
+    },
+    "0110": {
+        "name": "GSW",
+        "score": "0"
+    },
+    "0111": {
+        "name": "SAS",
+        "score": "0"
+    },
+
+    "1000": {
+        "name": "TOR",
+        "score": "0"
+    },
+    "1001": {
+        "name": "WAS",
+        "score": "0"
+    },
+    "1010": {
+        "name": "CLE",
+        "score": "0"
+    },
+    "1011": {
+        "name": "IND",
+        "score": "0"
+    },
+    "1100": {
+        "name": "PHI",
+        "score": "0"
+    },
+    "1101": {
+        "name": "MIA",
+        "score": "0"
+    },
+    "1110": {
+        "name": "BOS",
+        "score": "0"
+    },
+    "1111": {
+        "name": "MIL",
+        "score": "0"
+    },
+
+    "000": {
+        "name": "",
+        "score": "0"
+    },
+    "001": {
+        "name": "",
+        "score": "0"
+    },
+    "010": {
+        "name": "",
+        "score": "0"
+    },
+    "011": {
+        "name": "",
+        "score": "0"
+    },
+    "100": {
+        "name": "",
+        "score": "0"
+    },
+    "101": {
+        "name": "",
+        "score": "0"
+    },
+    "110": {
+        "name": "",
+        "score": "0"
+    },
+    "111": {
+        "name": "",
+        "score": "0"
+    },
+
+    "00": {
+        "name": "",
+        "score": "0"
+    },
+    "01": {
+        "name": "",
+        "score": "0"
+    },
+    "10": {
+        "name": "",
+        "score": "0"
+    },
+    "11": {
+        "name": "",
+        "score": "0"
+    },
+
+    "0": {
+        "name": "",
+        "score": "0"
+    },
+    "1": {
+        "name": "",
+        "score": "0"
+    },
+
+    "root": {
+        "name": "",
+        "score": "4"
+    }
+};
+
+var map = $.extend(true, {}, map_init);
+
+/*
+ * map serial is the brief way to store map object
+ *
+ * store is one of {0, 1, 2, 3, 4, q}
+ * q means store ?
+ *
+ * first 16 number means 0000 -> 1111 store
+ * next 8 number means 000 -> 111 store
+ * next 4 number means 00 -> 11 store
+ * next 2 number means 0 -> 1 store
+ * total 30 numbers
+ */
+// var map_serial = '000000000000000000000000000000';
+
+function serialize_map(map_obj) {
+    var serial = "";
+
+    for (var round = 4; round > 0; round--) {
+        var team_count = Math.pow(2, round);
+        for(var team = 0; team < team_count; team += 1) {
+            var pos = dec2bin(team, round);
+            var score = map_obj[pos]['score'];
+
+            if (score == '?') {
+                score = 'q';
+            }
+            serial += score;
+        }
+    }
+
+    return serial
+;
+}
+
+function unserialize_map(map_init_obj, map_str) {
+    if (map_str.length < 30 || (! /^[01234q]+$/.test(map_str))) {
+        return map_init_obj;
+    }
+
+    var map_obj = $.extend(true, {}, map_init_obj);
+    var index = 0;
+    for (var round = 4; round > 0; round--) {
+        var team_count = Math.pow(2, round);
+        for(var team = 0; team < team_count; team += 1) {
+            var pos = dec2bin(team, round);
+            var score = map_str[index];
+            if (score == 'q') {
+                score = '?';
+            }
+            map_obj[pos]['score'] = score;
+
+            index++;
+        }
+    }
+
+    return map_obj;
+}
+
+/*
+ * dec number to binary number in string form
+ * num, dec number
+ * len, the least result string length,
+ *    padding 0 if too short
+ * e.g.
+ *    dec2bin(10, 4)  =>   "1010"
+ *    dec2bin(20, 4)  =>   "10100"
+ *    dec2bin(1, 4)  =>   "0001"
+ */
+function dec2bin(num, len) {
+    var bin = num.toString(2);
+    var bin_len = bin.length;
+    if (bin_len >= len) {
+        return bin;
+    }
+
+    var padding = "0".repeat(len) + bin;
+    return padding.slice(-len);
+}
+
+function logo_link(team) {
+    if (team == "") {
+        return 'assets/logos/nba-logo-2.png';
+    }
+    return 'assets/logos/' + team + '_logo.png';
+}
+
+function score_link(score) {
+    if (score == '?') {
+        return 'assets/logos/score_q.png';
+    }
+    return 'assets/logos/score_' + score + '.png';
+}
+
+function link(asset) {
+    return 'assets/logos/' + asset + '.png';
+}
+
+function get_loser(pos) {
+    var last = pos.slice(-1);
+    var parent = pos.slice(0, -1);
+    if (last == '0') {
+        return parent + '1';
+    } else {
+        return parent + '0';
+    }
+}
+
+function get_parent_pos(pos) {
+    var parent = pos.slice(0, -1);
+    if (parent == "") {
+        parent = 'root';
+    }
+    return parent;
+}
+
+function reload() {
+    // conduct data map for bottom to top
+    for (var round = 4; round > 0; round--) {
+        var team_count = Math.pow(2, round);
+        for(var team = 0; team < team_count; team += 1) {
+            var pos = dec2bin(team, round);
+            var op_pos = get_loser(pos);
+            var parent_pos = get_parent_pos(pos);
+
+            var name = map[pos]['name'];
+            var score = map[pos]['score'];
+            var op_name = map[op_pos]['name'];
+            var op_score = map[op_pos]['score'];
+            var parent_name = map[parent_pos]['name'];
+            var parent_score = map[parent_pos]['score'];
+
+            if (name == "") {
+                continue;
+            }
+            if (op_name == "") {
+                if (parent_name != "") {
+                    map[parent_pos]['name'] = name;
+                }
+            }
+            if (score == '4') {
+                map[parent_pos]['name'] = name;
+            }
+        }
+    }
+
+    // set image
+    $.each(map, function(pos, val) {
+        var name = map[pos]["name"];
+        var score = map[pos]["score"];
+
+        var ele = $('#' + pos).svg();
+        ele.attr('xlink:href', logo_link(name));
+        var score_ele = $('#' + pos + '_score').svg();
+        score_ele.attr('xlink:href', score_link(score));
+    });
+
+    // set crown and basketball
+    var champion = 'root';
+    var basketball = $('#basketball').svg();
+    var crown = $('#crown').svg();
+    if (map[champion]['name'] == "") {
+        basketball.attr('xlink:href', link('blank'));
+        crown.attr('xlink:href', link('blank'));
+    } else {
+        basketball.attr('xlink:href', link('basketball'));
+        crown.attr('xlink:href', link('crown'));
+    }
+
+    // set url query
+    var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?map=' + serialize_map(map);
+    if (history.pushState) {
+        window.history.replaceState({path:newurl}, '', newurl);
+    }
+}
+
+function init() {
+    reload();
+
+    // bind click event
+    $('svg image').each(function(index, element) {
+        var team_ele = $(element).svg();
+
+        // only bind for team elements
+        var ele_regex = /^[01]{1,4}$/;
+        var id = $(element).attr('id');
+        if (! ele_regex.test(id)) {
+            return;
+        }
+
+        team_ele.click(function(event) {
+            var winner = $(event.target).attr('id');
+
+
+            var loser = get_loser(winner);
+            var parent = get_parent_pos(winner);
+
+            var winner_name = map[winner]['name'];
+            var loser_name = map[loser]['name'];
+
+            // if winner is nobody, return
+            // if loser is nobody, winner into next round, loser still nobody, score 4:?
+            // else popup to set score, if not set, do nothing
+            if (winner_name == "") {
+                return false;
+            }
+
+            var ele = $(event.target).svg();
+            var origin_w = ele.attr('width');
+            var origin_h = ele.attr('height');
+            var origin_x = ele.attr('x');
+            var origin_y = ele.attr('y');
+            ele.animate({svgWidth: +origin_w+10, svgHeight: +origin_h+10, svgX: +origin_x-5, svgY: +origin_y-5}, 50);
+            ele.animate({svgWidth: origin_w, svgHeight: origin_h, svgX: origin_x, svgY: origin_y}, 50);
+
+            if (loser_name == "") {
+                map[winner]["score"] = '4';
+                map[loser]["score"] = '?';
+                reload();
+                return false;
+            }
+
+            var modal = $('#vs').svg();
+            var vs = 'vs.svg';
+            if (isMobile()) {
+                vs = 'vs-mobile.svg';
+            }
+            modal.load(vs, function() {
+                $('#modal').modal();
+
+                var svg_root = $('#vs_root').svg();
+                svg_root.attr('width', '100%');
+                svg_root.attr('height', '100%');
+
+                // set winner loser logo
+                $('#vs_winner').attr('xlink:href', logo_link(map[winner]["name"]));
+                $('#vs_loser').attr('xlink:href', logo_link(map[loser]["name"]));
+
+                $('[id^=point_]').each(function(index, element) {
+                    var point_ele = $(element).svg();
+                    point_ele.click(function(event) {
+                        var point = $(event.target).attr('id').slice(-1);
+                        // animate
+                        var circle = $('#circle_' + point).svg();
+                        circle.animate({svgR: '+=10'}, 50);
+                        circle.animate({svgR: '-=10'}, 50);
+
+                        // q means question mark '?'
+                        if (point == 'q') {
+                            point = '?';
+                        }
+                        map[winner]["score"] = 4;
+                        map[loser]["score"] = point;
+                        reload();
+
+                        setTimeout(function() {
+                            $.modal.close();
+                        }, 100);
+                    });
+                });
+            });
+        });
+    });
+}
+
+function isMobile() {
+    return /Mobi/.test(navigator.userAgent);
+}
+
+// from https://css-tricks.com/snippets/javascript/get-url-variables/
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
+
+$(document).ready(function() {
+    // get query
+    var map_serial = getQueryVariable('map');
+    // set map
+    if (map_serial) {
+        map = unserialize_map(map_init, map_serial);
+    }
+
+    var container = $('#container').svg();
+    var schedule = 'schedule.svg';
+    if (isMobile()) {
+        schedule = 'schedule-mobile.svg';
+    }
+
+    container.load(schedule, function() {
+        var svg_root = $('#svg8').svg();
+        svg_root.attr('width', '100%');
+        svg_root.attr('height', '100%');
+
+        init();
+    });
+
+});
---
+++ readme.md
@@ -0,0 +1,117 @@
+
+
+grep  -o '/media.*logo.svg' nba-team-list.html  | awk '{print "http://china.nba.com" $0}'
+
+
+convert svg to png
+
+https://www.systutorials.com/241682/how-to-convert-svg-to-png-in-linux/
+
+javascript control svg
+
+http://www.petercollingridge.co.uk/data-visualisation/using-javascript-control-svg
+
+
+
+make favicon from cmd
+https://askubuntu.com/questions/867567/jpg-or-png-to-ico-using-terminal-and-back
+https://www.icoconverter.com/
+
+
+rel favicon
+https://stackoverflow.com/questions/18301745/how-to-set-up-a-favicon
+
+
+
+javascript float and string
+https://stackoverflow.com/questions/642650/how-to-convert-string-into-float-in-javascript
+
+
+
+webp compression
+
+https://itsfoss.com/webp-ubuntu-linux/
+
+
+compression jpeg and png
+
+https://www.tecmint.com/optimize-and-compress-jpeg-or-png-batch-images-linux-commandline/
+https://pngquant.org/
+https://www.fossmint.com/trimage-lossless-png-and-jpg-compressor-tool-linux/
+
+
+
+detect if browser is mobile browser
+https://stackoverflow.com/questions/3514784/what-is-the-best-way-to-detect-a-mobile-device-in-jquery
+
+
+
+renew dhcp ip address
+https://www.cyberciti.biz/faq/howto-linux-renew-dhcp-client-ip-address/
+
+
+identify font from image
+https://www.fontsquirrel.com/matcherator
+
+
+
+jquery change url without reload the page
+https://stackoverflow.com/questions/12508225/how-do-we-update-url-or-query-strings-using-javascript-jquery-without-reloading
+
+pushState vs replaceState
+https://stackoverflow.com/questions/17507091/replacestate-vs-pushstate?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+$.param()
+
+
+
+
+
+
+# design
+
+对阵图从数据层面来讲，是完全的二叉树结构
+
+root代表总冠军
+0代表西部冠军
+1代表东部冠军
+以此类推
+
+
+以0开始的都为西部球队，1开始的为东部球队
+
+对于任意一个编号节点，如0010，
+- 除去最后一个数字，剩下编号为其parent节点，这里是001（除去0，1节点，parent为root）
+- 本轮与之对战的对手，其节点编号只有最后一个的区别 0/1 变换，这里是0011
+
+每个节点有两个属性
+- name，球队的名称，谁取得了这项成就
+- score，在此轮和对手交手，所得比分，先得4分，球队将升至parent
+```
+{
+  "name": "",
+  "score": 0
+}
+```
+
+# config
+
+```json
+{
+  "map": {
+    "0000": {
+      "name": "",
+      "score": 0
+    }
+  }
+}
+```
+
+
+# todos
+
+- 在手机上，如果放大网页，则弹窗不对
+  - 在放大的前提下，居中显示，并自适应宽度
+  
+- 从链接中恢复页面
+  - 自己编辑再分享
